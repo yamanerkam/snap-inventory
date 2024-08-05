3cbcfcf91af0a28ce6d2ec243e4ab87d1ac9d6d9
Kam <erkamyaman35@gmail.com>
2024-08-05 21:55:02 +0300

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+defineCustomElements(window);
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
