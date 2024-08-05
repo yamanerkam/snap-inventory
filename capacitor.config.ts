9efe5ea27ff3ff5ad30ecadaca2016de76c77ed4
Kam <erkamyaman35@gmail.com>
2024-08-05 21:33:27 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'snap-inventory',
+  webDir: 'www'
+};
+
+export default config;
