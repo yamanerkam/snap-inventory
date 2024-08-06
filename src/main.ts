73745fcb4df15c0c488f1dbec6470eceda97d81d
Kam <erkamyaman35@gmail.com>
2024-08-07 00:23:51 +0300

    firebase-added

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 defineCustomElements(window);
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({"projectId":"snap-inventory-e69c6","appId":"1:564314679730:web:91f192636202cc7ec25cab","storageBucket":"snap-inventory-e69c6.appspot.com","apiKey":"AIzaSyCVcbGDXbdilyYTM2kygtvYwvm8o1jKX6s","authDomain":"snap-inventory-e69c6.firebaseapp.com","messagingSenderId":"564314679730"})), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()),
   ],
 });
